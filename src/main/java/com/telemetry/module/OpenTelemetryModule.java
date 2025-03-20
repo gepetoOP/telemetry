1a01aae42980cbb169408592d67200c95d986af4
Lucas de Rocha Castro <lucasrocha.castro@dtidigital.com.br>
2025-03-19 23:10:47 -0300

    refactor: junção de criação de classes de telemetria dentro do OpenTelemetry module + utilização de nome do serviço no lugar de escopo

--- src/main/java/com/telemetry/module/OpenTelemetryModule.java
+++ src/main/java/com/telemetry/module/OpenTelemetryModule.java
@@ -1,7 +1,11 @@
 package com.telemetry.module;
 
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.annotation.Value;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import jakarta.inject.Singleton;
 
@@ -11,5 +15,17 @@ public class OpenTelemetryModule {
     @Singleton
     public OpenTelemetry initOpenTelemetry() {
         return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+    }
+
+    @Singleton
+    @Requires(classes = OpenTelemetry.class)
+    public LongCounter getCounter(OpenTelemetry openTelemetry, @Value("${micronaut.application.name}") String applicationName) {
+        return openTelemetry.getMeter(applicationName).counterBuilder("api.calls").setDescription("Quantidade de chamadas de APIs").build();
+    }
+
+    @Singleton
+    @Requires(classes = OpenTelemetry.class)
+    public Tracer getTracer(OpenTelemetry openTelemetry, @Value("${micronaut.application.name}") String applicationName) {
+        return openTelemetry.getTracer(applicationName);
     }
 }
